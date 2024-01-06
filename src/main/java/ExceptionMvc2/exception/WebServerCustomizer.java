8d91686e3167b11627945f585895a1cdc3697009
keoni <kimkeonhwi991231@gmail.com>
2024-01-07 02:19:13 +0900

    first commit

---
+++ src/main/java/ExceptionMvc2/exception/WebServerCustomizer.java
@@ -0,0 +1,22 @@
+package ExceptionMvc2.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        //500 예외는 서버 내부에서 발생한 오류라는 뜻을 포함하고 있음.
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+
+
+    }
+}
